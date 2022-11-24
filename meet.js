449e58ba5db4fccbc4072ea42c452264dae7388f
Nathan Castle <nathan.castle@outlook.com>
2022-11-23 19:54:02 -0800

    Update with print, light/dark, responsive design

--- meet.js
+++ meet.js
@@ -1,131 +1,180 @@
-var enableObsidianLinks = true;
 var fileContents = "";
 var htmlOutput = null;
 
+class fileViewModel {
+  fileName = "";
+  rawContents = "";
+  filteredContents = "";
+  parsedContents = [];
+
+  mdForDisplay = null;
+  htmlForDisplay = null;
+  htmlForPrint = null;
+  useWikiLinksInMarkdown = false;
+
+  renderCallback = null;
+
+  reader = new FileReader();
+
+  constructor(renderCallback) {
+    this.renderCallback = renderCallback;
+    this.reader.onload = (event) => {
+      this.rawContents = event.target.result;
+      this.filteredContents = `${this.rawContents}`;
+      this.parseAndRender();
+    };
+  }
+
+  parseAndRender() {
+    this.parsedContents = this._convertToSchema(this.rawContents);
+    this.mdForDisplay = this._convertSchemaToMarkdown(this.parsedContents);
+    this.htmlForDisplay = this._convertSchemaToHtml(this.parsedContents);
+    this.htmlForPrint = this.htmlForDisplay.cloneNode(true);
+    this.renderCallback?.call();
+  }
+
+  _convertToSchema(inputText) {
+    let lines = inputText.split("\n");
+
+    let statements = [];
+    var statementInProgress = null;
+
+    let timeRegex = /^(([0-9]{2})\:){2}([0-9]{2})/g;
+
+    for (var line of lines) {
+      if (timeRegex.test(line)) {
+        if (statementInProgress != null) {
+          statementInProgress.contents = statementInProgress.contents.trim();
+          statements.push(statementInProgress);
+        }
+        statementInProgress = {
+          timestamp: "",
+          from: "",
+          to: "",
+          contents: "",
+        };
+        let fromParts = line.split(" From ", 2);
+        statementInProgress.timestamp = fromParts[0];
+        // TODO = handle cases where name contains ' to '
+        let toParts = fromParts[1].split(" to ");
+        statementInProgress.from = toParts[0];
+        statementInProgress.to = toParts[1].split(":")[0];
+      } else if (statementInProgress != null) {
+        statementInProgress.contents = `${
+          statementInProgress.contents
+        }\n${line.trim()}`;
+      }
+    }
+    return statements;
+  }
+
+  _convertSchemaToMarkdown(inputObj) {
+    var output = "";
+
+    for (var entry of inputObj) {
+      if (entry.to !== "Everyone") {
+        continue;
+      }
+
+      if (this.useWikiLinksInMarkdown === true) {
+        output = `${output}\n- [[${entry.from}]]: ${entry.contents.trim()}`;
+      } else {
+        output = `${output}\n- **${entry.from}**: (${
+          entry.timestamp
+        }) ${entry.contents.trim()}`;
+      }
+    }
+    return output;
+  }
+  _convertSchemaToHtml(inputObj) {
+    var outputRoot = document.createElement("div");
+    var unorderedList = document.createElement("ul");
+    outputRoot.appendChild(unorderedList);
+    for (var entry of inputObj) {
+      if (entry.to !== "Everyone") {
+        continue;
+      }
+      var li = document.createElement("li");
+      var fromSpan = document.createElement("strong");
+      fromSpan.innerText = entry.from;
+      li.appendChild(fromSpan);
+      let spacer = document.createElement("span");
+      spacer.innerHTML = "&nbsp;";
+      li.appendChild(spacer);
+      let timestamp = document.createElement("span");
+      timestamp.appendChild(document.createTextNode("("));
+      let timestampValue = document.createElement("time");
+      timestampValue.innerText = entry.timestamp;
+      timestamp.appendChild(timestampValue);
+      timestamp.appendChild(document.createTextNode("): "));
+      li.appendChild(timestamp);
+      let contents = document.createElement("span");
+      contents.innerText = entry.contents;
+      li.appendChild(contents);
+      unorderedList.appendChild(li);
+    }
+    return outputRoot;
+  }
+
+  reset() {
+    this.fileName = "";
+    this.rawContents = null;
+    thhis.parsedContents = [];
+  }
+
+  loadFromFile(file) {
+    this.fileName = file.name;
+    this.reader.readAsText(file);
+  }
+}
+
+let fileVM = new fileViewModel(renderContent);
+
 function handleObsidianLinkChange(evt) {
   enableObsidianLinks = evt.checked;
-  renderContent();
+  if (fileVM) {
+    fileVM.useWikiLinksInMarkdown = evt.checked
+    fileVM.parseAndRender();
+  }
 }
 
 function onChange() {
+  if (fileVM == null) {
+    fileVM = new fileViewModel(renderContent);
+  }
   const fileInput = document.getElementById("upload-button");
   const selectedFile = fileInput.files[0];
-
-  const outputView = document.getElementById("raw-file-contents");
-
-  var reader = new FileReader();
-  reader.onload = function (event) {
-    fileContents = event.target.result;
-    outputView.innerText = fileContents;
-    renderContent();
-  };
-  reader.readAsText(selectedFile);
+  fileVM.loadFromFile(selectedFile);
 }
+
 function renderContent() {
-  let processed = convertToSchema(fileContents);
-  let markdown = convertSchemaToMarkdown(processed);
+  document.title = `Zoom Chat: ${fileVM.fileName}`;
+  const rawOutput = document.getElementById("raw-file-contents");
   const markdownView = document.getElementById("markdown-contents");
-  markdownView.innerText = markdown;
+  const outputView = document.getElementById("html-output-view");
+  const printView = document.getElementById("html-print-view");
 
-  let htmlOutputelement = convertSchemaToHtml(processed);
-  let outputView = document.getElementById("html-output-view");
+  rawOutput.innerText = fileVM.rawContents;
+  markdownView.innerText = fileVM.mdForDisplay;
   outputView.innerHTML = "";
-  outputView.appendChild(htmlOutputelement);
-  htmlOutput = htmlOutputelement;
+  outputView.appendChild(fileVM.htmlForDisplay);
+  printView.innerHTML = "";
+  printView.appendChild(fileVM.htmlForPrint);
 }
 
-function convertSchemaToMarkdown(inputObj) {
-  var output = "";
-
-  for (var entry of inputObj) {
-    if (entry.to !== "Everyone") {
-      continue;
-    }
-
-    if (enableObsidianLinks === true) {
-      output = `${output}\n- [[${entry.from}]]: ${entry.contents.trim()}`;
-    } else {
-      output = `${output}\n- **${entry.from}**: (${
-        entry.timestamp
-      }) ${entry.contents.trim()}`;
-    }
-  }
-  return output;
+function handleCopyHtml(evt) {
+    handleCopy(evt, fileVM.htmlForDisplay.innerHTML)
 }
-
-function convertSchemaToHtml(inputObj) {
-  var outputRoot = document.createElement("div");
-  var unorderedList = document.createElement("ul");
-  outputRoot.appendChild(unorderedList);
-  for (var entry of inputObj) {
-    if (entry.to !== "Everyone") {
-      continue;
-    }
-    var li = document.createElement("li");
-    var fromSpan = document.createElement("strong");
-    fromSpan.innerText = entry.from;
-    li.appendChild(fromSpan);
-    let spacer = document.createElement("span");
-    spacer.innerHTML = "&nbsp;";
-    li.appendChild(spacer);
-    let timestamp = document.createElement("span");
-    timestamp.appendChild(document.createTextNode("("));
-    let timestampValue = document.createElement("time");
-    timestampValue.innerText = entry.timestamp;
-    timestamp.appendChild(timestampValue);
-    timestamp.appendChild(document.createTextNode("): "));
-    li.appendChild(timestamp);
-    let contents = document.createElement("span");
-    contents.innerText = entry.contents;
-    li.appendChild(contents);
-    unorderedList.appendChild(li);
-  }
-  return outputRoot;
+function handleCopyMd(evt){
+    handleCopy(evt, fileVM.mdForDisplay)
 }
-
-function convertToSchema(inputText) {
-  let lines = inputText.split("\n");
-
-  let statements = [];
-  var statementInProgress = null;
-
-  let timeRegex = /^(([0-9]{2})\:){2}([0-9]{2})/g;
-
-  for (var line of lines) {
-    if (timeRegex.test(line)) {
-      if (statementInProgress != null) {
-        statementInProgress.contents = statementInProgress.contents.trim();
-        statements.push(statementInProgress);
-      }
-      statementInProgress = {
-        timestamp: "",
-        from: "",
-        to: "",
-        contents: "",
-      };
-      let fromParts = line.split(" From ", 2);
-      statementInProgress.timestamp = fromParts[0];
-      // TODO = handle cases where name contains ' to '
-      let toParts = fromParts[1].split(" to ");
-      statementInProgress.from = toParts[0];
-      statementInProgress.to = toParts[1].split(":")[0];
-    } else if (statementInProgress != null) {
-      statementInProgress.contents = `${
-        statementInProgress.contents
-      }\n${line.trim()}`;
-    }
-  }
-  return statements;
-}
-
-async function handleCopyHtml(evt) {
-  console.dir(navigator.clipboard);
+async function handleCopy(evt, content) {
   if (navigator.clipboard) {
     try {
-        const blob = new Blob([htmlOutput.innerHTML], {type: 'text/html'})
+      const blob = new Blob([content], { type: "text/html" });
       await navigator.clipboard.write([
         new ClipboardItem({
-          'text/html': blob,
+          "text/html": blob,
         }),
       ]);
       return;
@@ -144,4 +193,6 @@ async function handleCopyHtml(evt) {
   document.execCommand("copy");
 }
 
-document.getElementById('copy-btn').addEventListener('click', handleCopyHtml);
+
+document.getElementById("copy-btn-html").addEventListener("click", handleCopyHtml);
+document.getElementById("copy-btn-md").addEventListener("click", handleCopyMd);
